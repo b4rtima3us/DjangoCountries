c3e2ceb901c927a4331728a362c6b1ffb608a595
G. Volk-Karachevskiy <bartima3us@yandex.ru>
2024-07-23 20:29:24 +0300

    add models

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Languages(models.Model):
+    country = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
